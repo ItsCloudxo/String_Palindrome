5b7443094aaab34a0b0f1ddca4c8a42734b5b2ac
ItsCloudxo <sebastian@nnsoft.dk>
2022-03-29 09:50:43 +0200

    Add project files.

---
+++ String_Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+static class StringHelper
+{
+    public static string ReverseString(string myStr)
+    {
+        char[] myArr = myStr.ToCharArray();
+        Array.Reverse(myArr);
+        return new string(myArr);
+    }
+}
+
+class StringPalindrome
+{
+    static void Main()
+    {
+        Console.WriteLine("Type a word here to see if it's a palindrome:");
+        string preRev = Console.ReadLine();
+        string rev = StringHelper.ReverseString(preRev);
+        
+        if (preRev.ToUpper() == rev.ToUpper())
+        {
+            Console.WriteLine($"Pre-reversed word: {preRev}. \n Reversed word: {rev}");
+            Console.WriteLine("The entered word is a palindrome");
+        }
+        else
+        {
+            Console.WriteLine($"Pre-reversed word: {preRev}. \n Reversed word: {rev}");
+            Console.WriteLine("The entered word is NOT a palindrome");
+        }
+    }
+}
